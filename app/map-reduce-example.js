c714011eca8b7491a54294b936f6827b25a887c8
Glenn J. Mason <glenn@glennji.com>
2016-05-30 16:09:24 +1000

    Add a map reduce example

--- app/map-reduce-example.js
+++ app/map-reduce-example.js
@@ -1,3 +1,19 @@
 /**
  * Created by glenn.mason on 27/05/2016.
  */
+function mapSummer(arr, mapper_fn, reducer_fn) {
+  var newArr = arr.map(mapper_fn);
+  var v = newArr.reduce(reducer_fn);
+  return v
+}
+
+var mapper = function (a) {
+  return 2 * a;
+};
+
+var reducer = function (a, b) {
+  return a + b;
+}
+
+a = [1, 2, 3, 4, 5];
+console.log(mapSummer(a, mapper, reducer));
